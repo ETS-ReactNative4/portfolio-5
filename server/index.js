5be25536cee88a45df5e4a989f9d09c26e670946
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2018-10-10 15:05:15 -0600

    added static

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 let express = require('express');
+const path = require('path');
 
 let app = express();
 
 app.listen(4000, () => {
     console.log("working on port 4000")
 })
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
