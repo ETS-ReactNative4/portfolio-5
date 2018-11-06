b6372bb7c942d8fb300d74ebe0ee0db47414040f
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2018-11-06 15:46:50 -0700

    build completed

--- server/index.js
+++ server/index.js
@@ -2,12 +2,13 @@ let express = require('express');
 const path = require('path');
 
 let app = express();
+app.use(express.static(path.join(__dirname, '../build')))
 
 app.listen(4000, () => {
     console.log("working on port 4000")
 })
 
 
-app.get('*', (req, res)=>{
-    res.sendFile(path.join(__dirname, '../build/index.html'));
-});
+// app.get('*', (req, res)=>{
+//     res.sendFile(path.join(__dirname, '../build/index.html'));
+// });
