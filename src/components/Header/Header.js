16d3404c7927d817e2be3380bec425aef0a48391
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2018-11-12 11:55:00 -0700

    worked on responsive header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,27 +6,69 @@ import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
 import './header.css'
 
-export default function Header(props){
-    return(
-        <div id="header">
+export default class Header extends Component{
 
-            <div id="header-left">
-                <Link to="/"><button>RUSSELL MEMORY</button></Link>
-            </div>
+    state = {
+        menuShown: false
+    }
 
-            <div id="header-right">
-                <label for="toggle">&#9776;</label>
-                <input type="checkbox" id="toggle"/>
 
-                <div class="menu">
-                    <div id="header-home"><Link to="/"><button>Home</button></Link></div>
-                    <div id="header-portfolio"><Link to="/portfolio"><button>Portfolio</button></Link></div>
-                    <div id="header-about"><Link to="/about"><button>About Me</button></Link></div>
+    onResize = () => {
+        if(window.innerWidth >= 700 && this.state.menuShown) {
+            this.closeMenu()
+        }
+    }
+
+    showMenu = () => {
+        if(!this.state.menuShown) {
+            window.addEventListener('resize', this.onResize)
+            this.setState({menuShown: true})
+        } else {
+            this.closeMenu()
+        }
+    }
+
+    closeMenu = () => {
+        window.removeEventListener('resize', this.onResize)
+        this.setState({menuShown: false})
+    }
+
+    render(){
+        return(
+            <div id="header">
+    
+                <div id="header-left">
+                    <Link to="/"><button>RUSSELL <span>MEMORY</span></button></Link>
                 </div>
+    
+                <div id="header-right">
+    
+                    
+                            <div id="header-home"><Link to="/"><button>Home</button></Link></div>
+                            <div id="header-portfolio"><Link to="/portfolio"><button>Portfolio</button></Link></div>
+                            <div id="header-about"><Link to="/about"><button>About <span>Me</span></button></Link></div>
+                    
+    
+                    
+                </div>
+                {/* <div id="header-right">
+                    <label onClick={this.showMenu} for="toggle">&#9776;</label>
+    
+                    {
+                        (this.state.menuShown || window.innerWidth > 700) &&
+                        <div className="menu">
+                            <div id="header-home"><Link to="/"><button>Home</button></Link></div>
+                            <div id="header-portfolio"><Link to="/portfolio"><button>Portfolio</button></Link></div>
+                            <div id="header-about"><Link to="/about"><button>About Me</button></Link></div>
+                        </div>
+                    }
+    
+                    
+                </div> */}
+    
             </div>
-
-        </div>
-    )
+        )
+    }
 }
 
 
