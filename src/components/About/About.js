c8102ce709bb4198371f3ce14f3a2f2cf4cc68a3
Russell Memory <rmemory@russells-mbp-2.lan>
2018-11-14 21:44:06 -0700

    updated font

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -20,9 +20,9 @@ export default function About(props){
                     </div>
 
                     <div id="about-paragraph">
-                        <p>I graduated from BYU in 2008 with a degree in Exercise Science. I joined a local tech startup when it had 15 employees and spent several years working there while it grew rapidly. It's now a multi-billion dollar company. It was exciting to see the growth and progress but it was also exciting to see the power of computer programming skills at work. That excitement made me want to learn how to program for myself.</p>
-                        <p>Whie I was growing up my dad worked for the CIA (true story!) so we moved a lot. I've lived in 6 different countries for at least 2 years each. I've also lived in 5 different states. Having experienced so many different cultures, I feel like I have the ability to get along well with all kinds of people. </p>
-                            <p>In my freetime I like to train for and compete in triathlons. I've finished an Ironman Triathlon and several shorter distance races as well. I also completed LOTOJA, a bike race which covers 206 miles and enters 3 different states in one day.</p>
+                        <p>After graduating from Brigham Young University in 2008 with a degree in Exercise Science, I joined a local tech startup when it had 15 employees. I worked there for several years while it grew rapidly. It's now a multi-billion dollar company. It was exciting to see its growth and progress and it was more exciting to see the power of computer programming skills at work. That experience made me want to learn how to program for myself.</p>
+                        <p>Growing up, my dad worked for the CIA (true story!) so we moved a lot. I've lived in 6 different countries for at least 2 years each. I've also lived in 5 states. Having experienced so many different cultures, I feel like I have the ability to get along well with all kinds of people.</p>
+                            <p>In my free time I like to train for triathlons. I've finished an Ironman Triathlon and several shorter distance races as well. I also completed LOTOJA, a bike race which covers 206 miles and enters 3 different states in one day.</p>
                     </div>
                 </div>
             </div>
